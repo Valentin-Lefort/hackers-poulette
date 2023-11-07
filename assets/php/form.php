443608cedc23893b5ed7f7f69121e49a1011261c
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-11-07 13:54:43 +0100

    add gender

--- assets/php/form.php
+++ assets/php/form.php
@@ -89,10 +89,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <div class="grid md:grid-cols-2 md:gap-6">
         <div class="relative z-0 w-full mb-6 group">
           <select name="selectInput" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer text-center">
-            <option value="select" hidden>Select a subject</option>
-            <option value="sub1">Subject 1</option>
-            <option value="sub2">Subject 2</option>
-            <option value="sub3">Subject 3</option>
+            <option value="select" hidden>Select a issue</option>
+            <option value="sub1">Account Issue</option>
+            <option value="sub2">Payment Issue</option>
+            <option value="sub3">Technical Problem</option>
+            <option value="sub4">In-game bug</option>
+            <option value="sub5">Report a player</option>
           </select>
           <label for="selectInput" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-colors1 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">Reason</label>
           <span class="error-message" id="subject-error"></span>
