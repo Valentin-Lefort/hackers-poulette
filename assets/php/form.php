e7ca5e2f4f08e927ff844669f8700f9d2a26cb8e
Valentin-Lefort <lefortvalentin94@gmail.com>
2023-11-07 12:26:41 +0100

    gender

--- assets/php/form.php
+++ assets/php/form.php
@@ -13,6 +13,9 @@ $countries = file_get_contents('assets/json/country.json');
 $countries = json_decode($countries, true);
 // var_dump($countries);
 
+$gender = file_get_contents('assets/json/gender.json');
+$gender = json_decode($gender, true);
+
 
 function sanitize($data)
 {
@@ -54,10 +57,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <div class="relative z-0 w-full mb-6 group">
         <select name="lastName" id="lastName" class="block text-center py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
         <option value="select">Select your Gender</option>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="undefined">Undefined</option>
+        <?php foreach ($gender as $genders) : ?>
+          <?php echo "<option value='"
+            . $genders['gender'] . "'>" . $genders['gender'] . "</option>"; ?>
+        <?php endforeach; ?>
         </select> <label for="floating_gender" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-colors1 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8">Gender</label>
       </div>
       <div class="relative z-0 w-full mb-6 group">
